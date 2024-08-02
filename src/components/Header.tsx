a245f47973ebe44f9006eead0187ed5c0be5ecaf
Nader <alannaderdev@gmail.com>
2024-08-02 12:56:09 -0600

    add header and wallet components

---
+++ src/components/Header.tsx
@@ -0,0 +1,50 @@
+"use client";
+import Link from "next/link";
+import WalletComponents from "@/components/WalletComponents";
+
+const Header: React.FC = () => {
+  return (
+    <header className="border-b-2 border-white text-white py-4">
+      <nav className="container mx-auto flex justify-between items-center">
+        <div className="text-lg font-bold">
+          <Link href="/">HomeBase</Link>
+        </div>
+        <ul className="flex space-x-4 items-center">
+          <li>
+            <Link
+              href="https://www.base.org/"
+              className="hover:underline"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Base
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/test"
+              className="hover:underline"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Apps
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="https://base.blockscout.com"
+              className="hover:underline"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Explorer
+            </Link>
+          </li>
+          <WalletComponents />
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/constants/constants.ts
@@ -0,0 +1,75 @@
+export const memeCoins = [
+  {
+    name: "Brett",
+    address: "",
+    coingeckoName: "brett-2",
+    image:
+      "https://assets.coingecko.com/coins/images/35529/standard/1000050750.png?1709031995",
+  },
+  {
+    name: "Toshi",
+    address: "",
+    coingeckoName: "toshi",
+    image:
+      "https://assets.coingecko.com/coins/images/31126/standard/Toshi_Logo_-_Circular.png?1721677476",
+  },
+  {
+    name: "Degen",
+    address: "",
+    coingeckoName: "degen-base",
+    image:
+      "https://assets.coingecko.com/coins/images/34515/standard/android-chrome-512x512.png?1706198225",
+  },
+  {
+    name: "Higher",
+    address: "",
+    coingeckoName: "higher",
+    image:
+      "https://assets.coingecko.com/coins/images/36084/standard/200x200logo.png?1710427814",
+  },
+  {
+    name: "mfercoin",
+    address: "",
+    coingeckoName: "mfercoin",
+    image:
+      "https://assets.coingecko.com/coins/images/36550/standard/mfercoin-logo.png?1711876821",
+  },
+];
+
+export const nftProjects = [
+  {
+    name: "Onchain Gaias",
+    address: "",
+    openseaName: "",
+    image:
+      "https://i.seadn.io/s/raw/files/0051dce5929fe4f195bf98bb20ea7aa0.jpg?auto=format&dpr=1&w=64",
+  },
+  {
+    name: "Base Gods",
+    address: "",
+    openseaName: "",
+    image:
+      "https://i.seadn.io/s/raw/files/5a0d8b395d0288ad816e302ecce9f3cd.gif?auto=format&dpr=1&w=64",
+  },
+  {
+    name: "Based Punks",
+    address: "",
+    openseaName: "",
+    image:
+      "https://i.seadn.io/s/raw/files/857e39e54f4fa53eb78d1747e9470478.png?auto=format&dpr=1&w=64",
+  },
+  {
+    name: "BasePaint",
+    address: "",
+    openseaName:
+      "https://i.seadn.io/gcs/files/bb9ca4cd26ea64203f696f42bcdf4bec.png?auto=format&dpr=1&w=64",
+    image: "",
+  },
+  {
+    name: "swatches by jvmi",
+    address: "",
+    openseaName: "",
+    image:
+      "https://i.seadn.io/s/raw/files/7de0794e14fbf7af9c135a9a52452500.gif?auto=format&dpr=1&w=64",
+  },
+];
