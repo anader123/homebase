1be9390c626a27dd4dc23caf23960db0f9c28576
Nader <alannaderdev@gmail.com>
2024-08-09 11:42:26 -0600

    add moshi mint link

--- src/app/api/moshicam/route.ts
+++ src/app/api/moshicam/route.ts
@@ -40,7 +40,7 @@ export async function POST(req: NextRequest): Promise<Response> {
         {
           action: "tx",
           label: `Mint Moshicam`,
-          target: "",
+          target: `${process.env.NEXT_PUBLIC_BASE_URL}/api/mint`,
         },
       ],
       image: {
